8a35cf3158896430abb648fb135c7c592a8c8381
Mattia Massarenti <mattia.mas2003@gmail.com>
2025-11-05 09:57:53 +0100

    working on first program

---
+++ 01/array_sum.cu
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <math.h>
+
+
+
+int main() {
+    int N = 1 << 20;      // It shifts 1 to left 20 times => N = 2^20 = 1048576 (faster than using a function which computes the power)
+}
