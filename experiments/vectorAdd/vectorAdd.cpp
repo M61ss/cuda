b4fc3e1546fd1bacb8361d1f84ad922dbde58714
Mattia Massarenti <mattia.mas2003@gmail.com>
2025-11-20 14:06:45 +0100

    feat: implement vector addition functionality in vectorAdd.cpp

---
+++ experiments/vectorAdd/vectorAdd.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <math.h>
+
+void vectorAdd(float *v, float *u, float *res, const int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        res[i] = v[i] + u[i];
+    }
+}
+
+int main(void)
+{
+    const int N = 1 << 20;
+    // float v[N], u[N], y[N], x[N];    ERROR: Setting N too high causes stack overflow creating arrays
+    //                                         (the stack is 1MB), so I am using vectors
+    float *v = (float *)malloc(N * sizeof(float));
+    float *u = (float *)malloc(N * sizeof(float));
+    float *y = (float *)malloc(N * sizeof(float));
+    float *x = (float *)malloc(N * sizeof(float));
+
+    for (int i = 0; i < N; i++)
+    {
+        v[i] = 1.0f;
+        u[i] = 1.0f;
+        y[i] = 2.0f;
+    }
+
+    vectorAdd(v, u, x, N);
+
+    float maxError = 0;
+    for (int i = 0; i < N; i++)
+    {
+        maxError = fmaxf(0, fabsf(x[i] - y[i]));
+    }
+    std::cout << "Maximum detected error: " << maxError << std::endl;
+
+    free(v);
+    free(u);
+    free(x);
+    free(y);
+
+    exit(0);
+}
