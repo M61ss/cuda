4cfd5bf49fcada883f62cbd4d960854f4472aca2
Mattia Massarenti <mattia.mas2003@gmail.com>
2025-11-21 17:24:19 +0100

    compute and print elapsed times for operations

--- experiments/vectorAdd/vectorAdd.cu
+++ experiments/vectorAdd/vectorAdd.cu
@@ -1,4 +1,11 @@
 #include <iostream>
+#include <chrono>
+
+void computeElapsedTime(std::chrono::_V2::system_clock::time_point begin, std::chrono::_V2::system_clock::time_point end, std::string msg)
+{
+    auto elapsed = (float)std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() / 1000;
+    std::cout << msg << ": " << elapsed << "ms" << std::endl;
+}
 
 __global__ void vectorInit(float *v, float value)
 {
@@ -15,11 +22,17 @@ __global__ void vectorAdd(float *v1, float *v2, float *result)
 int main(void)
 {
     const int N = 1 << 20;
+    auto clock = std::chrono::high_resolution_clock();
+    auto init = clock.now();
+
+    auto begin = clock.now();
     float *v, *u, *label, *result;
     cudaMallocManaged(&v, N * sizeof(float));
     cudaMallocManaged(&u, N * sizeof(float));
     cudaMallocManaged(&label, N * sizeof(float));
     cudaMallocManaged(&result, N * sizeof(float));
+    auto end = clock.now();
+    computeElapsedTime(begin, end, "Unified Memory allocation time");
 
     cudaMemLocation loc;
     loc.id = 0;
@@ -30,15 +43,21 @@ int main(void)
     int numThreads = 256;
     int numBlocks = (N + numThreads - 1) / numThreads;
 
+    begin = clock.now();
     vectorInit<<<numBlocks, numThreads>>>(v, 1.0f);
     cudaDeviceSynchronize();
     vectorInit<<<numBlocks, numThreads>>>(u, 2.0f);
     cudaDeviceSynchronize();
     vectorInit<<<numBlocks, numThreads>>>(label, 3.0f);
     cudaDeviceSynchronize();
+    end = clock.now();
+    computeElapsedTime(begin, end, "Vector initialization time");
 
+    begin = clock.now();
     vectorAdd<<<numBlocks, numThreads>>>(v, u, result);
     cudaDeviceSynchronize();
+    end = clock.now();
+    computeElapsedTime(begin, end, "Vector sum time");
 
     float maxError = 0;
     for (int i = 0; i < N; i++) {
@@ -50,5 +69,9 @@ int main(void)
     cudaFree(u);
     cudaFree(result);
     cudaFree(label);
+
+    end = clock.now();
+    computeElapsedTime(init, end, "Total running time");
+
     exit(EXIT_SUCCESS);
 }
