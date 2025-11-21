982c0df2621762e5a1ae98e633b73ac665334f07
Mattia Massarenti <mattia.mas2003@gmail.com>
2025-11-21 08:41:13 +0100

    create vectorAdd kernel

--- experiments/vectorAdd/vectorAdd.cu
+++ experiments/vectorAdd/vectorAdd.cu
@@ -4,6 +4,12 @@ __global__ void vectorInit(float *v, float value, const int length)
     v[i] = value;
 }
 
+__global__ void vectorAdd(float *v1, float *v2, float *result, const int length)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    result[i] = v1[i] + v2[i];
+}
+
 int main(void)
 {
     const int N = 1 << 20;
