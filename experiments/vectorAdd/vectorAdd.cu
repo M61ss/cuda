63cce5a31057abfb6367c3f84989d92a7598c2ad
Mattia Massarenti <mattia.mas2003@gmail.com>
2025-11-21 08:54:44 +0100

    complete vector sum in CUDA

--- experiments/vectorAdd/vectorAdd.cu
+++ experiments/vectorAdd/vectorAdd.cu
@@ -1,10 +1,12 @@
-__global__ void vectorInit(float *v, float value, const int length)
+#include <iostream>
+
+__global__ void vectorInit(float *v, float value)
 {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     v[i] = value;
 }
 
-__global__ void vectorAdd(float *v1, float *v2, float *result, const int length)
+__global__ void vectorAdd(float *v1, float *v2, float *result)
 {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     result[i] = v1[i] + v2[i];
@@ -18,6 +20,31 @@ int main(void)
     cudaMallocManaged(&u, N * sizeof(float));
     cudaMallocManaged(&label, N * sizeof(float));
     cudaMallocManaged(&result, N * sizeof(float));
+
+    cudaMemLocation loc;
+    loc.id = 0;
+    loc.type = cudaMemLocationTypeDevice;
+    cudaMemPrefetchAsync(v, N * sizeof(float), loc, 0);        
+    cudaMemPrefetchAsync(u, N * sizeof(float), loc, 0);     
+    cudaMemPrefetchAsync(result, N * sizeof(float), loc, 0);
+    int numThreads = 256;
+    int numBlocks = (N + numThreads - 1) / numThreads;
+
+    vectorInit<<<numBlocks, numThreads>>>(v, 1.0f);
+    cudaDeviceSynchronize();
+    vectorInit<<<numBlocks, numThreads>>>(u, 2.0f);
+    cudaDeviceSynchronize();
+    vectorInit<<<numBlocks, numThreads>>>(label, 3.0f);
+    cudaDeviceSynchronize();
+
+    vectorAdd<<<numBlocks, numThreads>>>(v, u, result);
+    cudaDeviceSynchronize();
+
+    float maxError = 0;
+    for (int i = 0; i < N; i++) {
+        maxError = fmaxf(0, fabsf(result[i] - label[i]));
+    }
+    std::cout << "Maximum detected error: " << maxError << std::endl;
 
     cudaFree(v);
     cudaFree(u);
