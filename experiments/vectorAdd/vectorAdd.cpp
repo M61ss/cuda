785478fde4530fa176a5c20e3e65184cbfd09ed0
Mattia Massarenti <mattia.mas2003@gmail.com>
2025-11-21 07:36:12 +0100

    refact variable names

--- experiments/vectorAdd/vectorAdd.cpp
+++ experiments/vectorAdd/vectorAdd.cpp
@@ -16,29 +16,29 @@ int main(void)
     //                                         (the stack is 1MB), so I am using vectors
     float *v = (float *)malloc(N * sizeof(float));
     float *u = (float *)malloc(N * sizeof(float));
-    float *y = (float *)malloc(N * sizeof(float));
-    float *x = (float *)malloc(N * sizeof(float));
+    float *label = (float *)malloc(N * sizeof(float));
+    float *result = (float *)malloc(N * sizeof(float));
 
     for (int i = 0; i < N; i++)
     {
         v[i] = 1.0f;
         u[i] = 1.0f;
-        y[i] = 2.0f;
+        label[i] = 2.0f;
     }
 
-    vectorAdd(v, u, x, N);
+    vectorAdd(v, u, result, N);
 
     float maxError = 0;
     for (int i = 0; i < N; i++)
     {
-        maxError = fmaxf(0, fabsf(x[i] - y[i]));
+        maxError = fmaxf(0, fabsf(result[i] - label[i]));
     }
     std::cout << "Maximum detected error: " << maxError << std::endl;
 
     free(v);
     free(u);
-    free(x);
-    free(y);
+    free(result);
+    free(label);
 
     exit(EXIT_SUCCESS);
 }
