50644863a6aae56903c7cb257738c29092833247
Mattia Massarenti <mattia.mas2003@gmail.com>
2025-11-22 10:23:20 +0100

    add length check

--- experiments/vectorAdd/vectorAdd.cu
+++ experiments/vectorAdd/vectorAdd.cu
@@ -9,16 +9,20 @@ void computeElapsedTime(std::chrono::_V2::system_clock::time_point begin, std::c
     *total_time += elapsed;
 }
 
-__global__ void vectorInit(float *v, float value)
+__global__ void vectorInit(float *v, float value, const int length)
 {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-    v[i] = value;
+
+    if (i < length)
+        v[i] = value;
 }
 
-__global__ void vectorAdd(float *v1, float *v2, float *result)
+__global__ void vectorAdd(float *v1, float *v2, float *result, const int length)
 {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-    result[i] = v1[i] + v2[i];
+
+    if (i < length)
+        result[i] = v1[i] + v2[i];
 }
 
 int main(void)
@@ -106,21 +110,21 @@ int main(void)
 
     begin = std::chrono::high_resolution_clock::now();
 
-    vectorInit<<<numBlocks, numThreads>>>(v, 1.0f);
+    vectorInit<<<numBlocks, numThreads>>>(v, 1.0f, N);
 
     if (error != cudaSuccess) {
         fprintf(stderr, "Failed to initialize vector 'v' on device. Code %s", cudaGetErrorString(error));
         exit(EXIT_FAILURE);
     }
 
-    vectorInit<<<numBlocks, numThreads>>>(u, 2.0f);
+    vectorInit<<<numBlocks, numThreads>>>(u, 2.0f, N);
 
     if (error != cudaSuccess) {
         fprintf(stderr, "Failed to initialize vector 'u' on device. Code %s", cudaGetErrorString(error));
         exit(EXIT_FAILURE);
     }
 
-    vectorInit<<<numBlocks, numThreads>>>(label, 3.0f);
+    vectorInit<<<numBlocks, numThreads>>>(label, 3.0f, N);
 
     if (error != cudaSuccess) {
         fprintf(stderr, "Failed to initialize vector 'label' on device. Code %s", cudaGetErrorString(error));
@@ -140,7 +144,7 @@ int main(void)
     // VECTOR SUM
     begin = std::chrono::high_resolution_clock::now();
 
-    vectorAdd<<<numBlocks, numThreads>>>(v, u, result);
+    vectorAdd<<<numBlocks, numThreads>>>(v, u, result, N);
 
     if (error != cudaSuccess) {
         fprintf(stderr, "Failed to compute v+u on device. Code %s", cudaGetErrorString(error));
