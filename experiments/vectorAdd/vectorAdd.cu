328304796b5e49c14199b045d501d008b2507d07
Mattia Massarenti <mattia.mas2003@gmail.com>
2025-11-22 10:19:29 +0100

    handle errors

--- experiments/vectorAdd/vectorAdd.cu
+++ experiments/vectorAdd/vectorAdd.cu
@@ -5,6 +5,7 @@ void computeElapsedTime(std::chrono::_V2::system_clock::time_point begin, std::c
 {
     auto elapsed = (float)std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() / 1000;
     std::cout << msg << ": " << elapsed << "ms" << std::endl;
+
     *total_time += elapsed;
 }
 
@@ -26,41 +27,132 @@ int main(void)
     // auto clock = std::chrono::high_resolution_clock();       It causes warning "variable never used" because nvcc doesn't understand 
     //                                                          completly C++ type creation (nvcc has many limitations in C++ frontend,
     //                                                          so avoid fancy implementations)
+    cudaError_t error = cudaSuccess;
+    
     float total_time = 0.0f;
 
+    // MEMORY ALLOCATION
     auto begin = std::chrono::high_resolution_clock::now();
+
     float *v, *u, *label, *result;
+
     cudaMallocManaged(&v, N * sizeof(float));
+
+    if (error != cudaSuccess) {
+        fprintf(stderr, "Failed to allocate vector 'v' on Unified Memory. Code %s", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }
+
     cudaMallocManaged(&u, N * sizeof(float));
+
+    if (error != cudaSuccess) {
+        fprintf(stderr, "Failed to allocate vector 'u' on Unified Memory. Code %s", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }
+
     cudaMallocManaged(&label, N * sizeof(float));
+
+    if (error != cudaSuccess) {
+        fprintf(stderr, "Failed to allocate vector 'label' on Unified Memory. Code %s", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }
+
     cudaMallocManaged(&result, N * sizeof(float));
+
+    if (error != cudaSuccess) {
+        fprintf(stderr, "Failed to allocate vector 'result' on Unified Memory. Code %s", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }
+
     auto end = std::chrono::high_resolution_clock::now();
     computeElapsedTime(begin, end, "Unified Memory allocation time", &total_time);
 
+    // MEMORY PREFETCH
     cudaMemLocation loc;
     loc.id = 0;
     loc.type = cudaMemLocationTypeDevice;
-    cudaMemPrefetchAsync(v, N * sizeof(float), loc, 0);        
-    cudaMemPrefetchAsync(u, N * sizeof(float), loc, 0);     
+
+    cudaMemPrefetchAsync(v, N * sizeof(float), loc, 0);
+
+    if (error != cudaSuccess) {
+        fprintf(stderr, "Failed to prefetch vector 'v' on device. Code %s", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }        
+
+    cudaMemPrefetchAsync(u, N * sizeof(float), loc, 0);
+
+    if (error != cudaSuccess) {
+        fprintf(stderr, "Failed to prefetch vector 'u' on device. Code %s", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }      
+
     cudaMemPrefetchAsync(result, N * sizeof(float), loc, 0);
+
+    if (error != cudaSuccess) {
+        fprintf(stderr, "Failed to prefetch vector 'result' on device. Code %s", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }  
+
+    // VECTOR INITIALIZATION
     int numThreads = 256;
     int numBlocks = (N + numThreads - 1) / numThreads;
 
     begin = std::chrono::high_resolution_clock::now();
+
     vectorInit<<<numBlocks, numThreads>>>(v, 1.0f);
+
+    if (error != cudaSuccess) {
+        fprintf(stderr, "Failed to initialize vector 'v' on device. Code %s", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }
+
     vectorInit<<<numBlocks, numThreads>>>(u, 2.0f);
+
+    if (error != cudaSuccess) {
+        fprintf(stderr, "Failed to initialize vector 'u' on device. Code %s", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }
+
     vectorInit<<<numBlocks, numThreads>>>(label, 3.0f);
+
+    if (error != cudaSuccess) {
+        fprintf(stderr, "Failed to initialize vector 'label' on device. Code %s", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }
+
     cudaDeviceSynchronize();
+
+    if (error != cudaSuccess) {
+        fprintf(stderr, "Failed to sync device with host. Code %s", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }
+
     end = std::chrono::high_resolution_clock::now();
     computeElapsedTime(begin, end, "Vector initialization time", &total_time);
 
+    // VECTOR SUM
     begin = std::chrono::high_resolution_clock::now();
+
     vectorAdd<<<numBlocks, numThreads>>>(v, u, result);
+
+    if (error != cudaSuccess) {
+        fprintf(stderr, "Failed to compute v+u on device. Code %s", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }
+
     cudaDeviceSynchronize();
+
+    if (error != cudaSuccess) {
+        fprintf(stderr, "Failed to sync device with host. Code %s", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }
+
     end = std::chrono::high_resolution_clock::now();
     computeElapsedTime(begin, end, "Vector sum time", &total_time);
 
+    // COMPUTE NUMERICAL ERROR
     float maxError = 0;
+
     for (int i = 0; i < N; i++) {
         maxError = fmaxf(0, fabsf(result[i] - label[i]));
     }
@@ -68,9 +160,34 @@ int main(void)
 
     std::cout << "Total elapsed time: " << total_time << "ms" << std::endl;
 
+    // FREE MEMORY
     cudaFree(v);
+
+    if (error != cudaSuccess) {
+        fprintf(stderr, "Failed to free 'v' memory. Code %s", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }
+
     cudaFree(u);
+
+    if (error != cudaSuccess) {
+        fprintf(stderr, "Failed to free 'u' memory. Code %s", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }
+
     cudaFree(result);
+
+    if (error != cudaSuccess) {
+        fprintf(stderr, "Failed to free 'result' memory. Code %s", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }
+
     cudaFree(label);
+
+    if (error != cudaSuccess) {
+        fprintf(stderr, "Failed to free 'label' memory. Code %s", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }
+
     exit(EXIT_SUCCESS);
 }
